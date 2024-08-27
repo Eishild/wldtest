ae76a097db042e09ffa66e5169c4c65d0cb12e0c
Eishild <jimmy.huet974@hotmail.fr>
2024-08-27 21:41:46 +0400

    first query db

--- src/pages/Job.tsx
+++ src/pages/Job.tsx
@@ -1,7 +1,32 @@
+import { get, getDatabase, ref } from "firebase/database"
+import app from "../firebaseConfig"
+import { useEffect, useState } from "react"
+
+interface jobType {
+  title: string
+  id: string
+}
+
 const Job = () => {
+  const [jobList, setJobList] = useState([])
+
+  const database = getDatabase(app)
+  const dbRef = ref(database)
+
+  useEffect(() => {
+    get(dbRef).then((el) => {
+      if (el.exists()) {
+        setJobList(el.val())
+      }
+    })
+  }, [])
+
   return (
     <main className="px-8 py-4">
       <p>Job</p>
+      {jobList.map((job: jobType) => (
+        <p key={job.id}>{job.title}</p>
+      ))}
     </main>
   )
 }
